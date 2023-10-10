f4f913145cc9a32182d3a55cd577de6fb96b8bb3
Kaushalya Hulathdoowage <skdilukshi97@gmail.com>
2023-10-11 00:07:42 +0530

    modified

--- src/ComponentsGeneral/ContactUS/ContactUS.jsx
+++ src/ComponentsGeneral/ContactUS/ContactUS.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import './ContactUS.css';
+import LocationSrc from './Assets/LocationSrc';
+
+
 
 function ContactUs() {
   return (
     <div className='ContactUs-container'>
+        <div>
         <h1 className='Maintopic-general-PO'>CONTACT US</h1>
-        <div></div>
+        </div>
+        <div className='topic-line'></div>
+        <div>
+          <div>
+            ?<img src={LocationSrc} alt='location' className='Location-image'/>
+          </div>
+        </div>
     </div>
   );
 };
